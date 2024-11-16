240ae2f00bc33322edbc3cd0157f9a00778547ed
s0wcy <alexandrechabeau.pro@gmail.com>
2024-11-16 12:55:22 +0700

    :bug: Fixed Web3Auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { useWeb3Auth } from "@/context/Web3AuthContext"
 import { Button } from "@/components/Button"
 
 export const Header = () => {
-  const { provider, web3auth, login, logout, loggedIn } = useWeb3Auth()
+  const { login, logout, loggedIn } = useWeb3Auth()
 
   return (
     <header className='flex flex-row justify-between items-center w-screen h-[80px] px-[16px] absolute top-0 left-0'>
