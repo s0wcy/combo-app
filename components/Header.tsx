51ee4a0857555d0ae70129697341df0aa171229f
s0wcy <alexandrechabeau.pro@gmail.com>
2024-11-16 16:13:40 +0700

    :bug: Fix naming conflict

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,9 @@
-import { useWeb3Auth } from "@/context/Web3AuthContext"
+import { useWeb3 } from "@/context/Web3Context"
 
 import { Button } from "@/components/Button"
 
 export const Header = () => {
-  const { login, logout, loggedIn } = useWeb3Auth()
+  const { login, logout, loggedIn } = useWeb3()
 
   return (
     <header className='flex flex-row justify-between items-center w-screen h-[80px] px-[16px] absolute top-0 left-0'>
